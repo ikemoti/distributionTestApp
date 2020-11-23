da9e826e922d6ecec968f0ef19dbacf74b0edf65
“ikemoti” <ikepon413@icloud.com>
2020-11-24 08:57:59 +0900

    Initial Commit

---
+++ distributionTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  distributionTestApp
+//
+//  Created by Sousuke Ikemoto on 2020/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
